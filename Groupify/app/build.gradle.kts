dfa1678f41a1c18b6e30e8c88f08de75abb0d799
JeongHyeon Lee <jlh1218@naver.com>
2024-07-30 13:22:27 +0900

    connect to Firebase

--- Groupify/app/build.gradle.kts
+++ Groupify/app/build.gradle.kts
@@ -75,8 +75,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-analytics:22.0.2")
     implementation("com.google.firebase:firebase-storage:21.0.0")
-    implementation("com.google.firebase:firebase-firestore:25.0.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
+    implementation("com.google.firebase:firebase-storage-ktx:21.0.0")
+
+    implementation(kotlin("script-runtime"))
 }
--- Groupify/app/src/main/java/com/example/groupify/MainActivity.kt
+++ Groupify/app/src/main/java/com/example/groupify/MainActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.Settings
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
@@ -32,83 +33,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Firebase Storage 초기화
-        val storage = FirebaseStorage.getInstance("gs://groupify-d9e65.appspot.com")
+        val storage = FirebaseStorage.getInstance()
         storageRef = storage.reference
 
         // "Hello~" 텍스트뷰 설정
         val textView: TextView = findViewById(R.id.textView)
         textView.text = "Hello~"
 
-        // 모든 앱 로그 출력
-        val packageManager = packageManager
-        val packages = packageManager.getInstalledPackages(0)
-
-        for (packageInfo in packages) {
-            val appName = packageInfo.applicationInfo.loadLabel(packageManager).toString()
-            val packageName = packageInfo.packageName
-            val versionName = packageInfo.versionName
-            val versionCode = packageInfo.longVersionCode
-
-            // 로그 출력
-            Log.d("AllAppInfo", "App Name: $appName, Package Name: $packageName, Version Name: $versionName, Version Code: $versionCode")
-        }
-
         // 권한 확인 및 요청
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (!Environment.isExternalStorageManager()) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE), 1)
-            } else {
-                logInstalledApps()
-            }
-        } else {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-            } else {
-                logInstalledApps()
-            }
-        }
+        checkAndRequestPermissions()
 
         // 버튼 설정
         val buttonNext = findViewById<Button>(R.id.button_next)
         buttonNext.setOnClickListener {
             val intent = Intent(this, DeployActivity::class.java)
             startActivity(intent)
+        }
+    }
 
-            try {
-                // 앱 이름과 아이콘을 표시할 컨테이너
-                val appContainer: LinearLayout = findViewById(R.id.appContainer)
-                val packageManager = packageManager
-                val packages = packageManager.getInstalledPackages(0)
-
-                for (packageInfo in packages) {
-                    // 구글 플레이 스토어에서 설치된 앱만 필터링
-                    if (packageManager.getInstallerPackageName(packageInfo.packageName) == "com.android.vending") {
-                        val appName = packageInfo.applicationInfo.loadLabel(packageManager).toString()
-                        val appIcon = packageInfo.applicationInfo.loadIcon(packageManager)
-
-                        // 로그 출력
-                        Log.d("AppInfo", "App Name: $appName")
-
-                        // 동적으로 텍스트뷰와 이미지뷰 생성
-                        val textView = TextView(this)
-                        textView.text = appName
-
-                        val imageView = ImageView(this)
-                        imageView.setImageDrawable(appIcon)
-                        imageView.layoutParams = LinearLayout.LayoutParams(100, 100) // 아이콘 크기 설정
-
-                        // 컨테이너에 추가
-                        val appLayout = LinearLayout(this)
-                        appLayout.orientation = LinearLayout.HORIZONTAL
-                        appLayout.addView(imageView)
-                        appLayout.addView(textView)
-
-                        appContainer.addView(appLayout)
-                    }
-                }
-            } catch (e: Exception) {
-                Log.e("AppInfo", "Error retrieving app information", e)
+    private fun checkAndRequestPermissions() {
+        Log.d("PermissionCheck", "Checking permissions")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Log.d("PermissionCheck", "Running on Android 11 or higher")
+            if (!Environment.isExternalStorageManager()) {
+                Log.d("PermissionCheck", "Requesting MANAGE_EXTERNAL_STORAGE permission")
+                val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                startActivityForResult(intent, 1)
+            } else {
+                Log.d("PermissionCheck", "MANAGE_EXTERNAL_STORAGE permission already granted")
+                logInstalledApps()
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                Log.d("PermissionCheck", "Requesting WRITE_EXTERNAL_STORAGE and READ_EXTERNAL_STORAGE permissions")
+                ActivityCompat.requestPermissions(this, arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ), 1)
+            } else {
+                Log.d("PermissionCheck", "WRITE_EXTERNAL_STORAGE and READ_EXTERNAL_STORAGE permissions already granted")
+                logInstalledApps()
             }
         }
     }
@@ -116,40 +81,92 @@ class MainActivity : AppCompatActivity() {
     // 권한 요청 결과 처리
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d("PermissionResult", "Request code: $requestCode, Permissions: ${permissions.joinToString()}, GrantResults: ${grantResults.joinToString()}")
         if (requestCode == 1) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // 권한이 허용된 경우 로그를 저장
+                Log.d("PermissionResult", "WRITE_EXTERNAL_STORAGE permission granted")
                 logInstalledApps()
             } else {
                 // 권한이 거부된 경우
-                Log.e("Permission", "WRITE_EXTERNAL_STORAGE permission denied")
+                Log.e("PermissionResult", "WRITE_EXTERNAL_STORAGE permission denied")
+            }
+        }
+    }
+
+    // Android 11 이상에서의 권한 결과 처리
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("ActivityResult", "Request code: $requestCode, Result code: $resultCode")
+        if (requestCode == 1) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                if (Environment.isExternalStorageManager()) {
+                    Log.d("ActivityResult", "MANAGE_EXTERNAL_STORAGE permission granted")
+                    logInstalledApps()
+                } else {
+                    Log.e("ActivityResult", "MANAGE_EXTERNAL_STORAGE permission denied")
+                }
             }
         }
     }
 
     private fun logInstalledApps() {
+        Log.d("LogApps", "Logging installed apps")
         val packageManager = packageManager
         val packages = packageManager.getInstalledPackages(0)
         val logStringBuilder = StringBuilder()
+        val googlePlayLogStringBuilder = StringBuilder()
 
         for (packageInfo in packages) {
             val appName = packageInfo.applicationInfo.loadLabel(packageManager).toString()
             val packageName = packageInfo.packageName
             val versionName = packageInfo.versionName
             val versionCode = packageInfo.longVersionCode
+            val appContainer: LinearLayout = findViewById(R.id.appContainer)
 
-            // 로그 문자열 생성
+            // 모든 앱의 로그 문자열 생성
             logStringBuilder.append("App Name: $appName, Package Name: $packageName, Version Name: $versionName, Version Code: $versionCode\n")
 
-            // 로그 출력 (AllAppInfo 태그 사용)
-            Log.d("AllAppInfo", "App Name: $appName, Package Name: $packageName, Version Name: $versionName, Version Code: $versionCode")
+            // 모든 앱 로그 출력 (AllAppInfo 태그 사용)
+            Log.d("AllAppList", "App Name: $appName, Package Name: $packageName, Version Name: $versionName, Version Code: $versionCode")
+
+
+
+            // 구글 플레이 스토어에서 설치된 앱만 필터링
+            if (packageManager.getInstallerPackageName(packageInfo.packageName) == "com.android.vending") {
+                val appName = packageInfo.applicationInfo.loadLabel(packageManager).toString()
+                val appIcon = packageInfo.applicationInfo.loadIcon(packageManager)
+
+                // 동적으로 텍스트뷰와 이미지뷰 생성
+                val textView = TextView(this)
+                textView.text = appName
+
+                val imageView = ImageView(this)
+                imageView.setImageDrawable(appIcon)
+                imageView.layoutParams = LinearLayout.LayoutParams(100, 100) // 아이콘 크기 설정
+
+                // 컨테이너에 추가
+                val appLayout = LinearLayout(this)
+                appLayout.orientation = LinearLayout.HORIZONTAL
+                appLayout.addView(imageView)
+                appLayout.addView(textView)
+
+                appContainer.addView(appLayout)
+                // 구글 플레이 스토어에서 설치된 앱의 로그 문자열 생성
+                googlePlayLogStringBuilder.append("App Name: $appName, Package Name: $packageName, Version Name: $versionName, Version Code: $versionCode\n")
+
+                // 구글 플레이 스토어에서 설치된 앱 로그 출력 (AppInfo 태그 사용)
+                Log.d("AppInfo", "App Name: $appName, Package Name: $packageName, Version Name: $versionName, Version Code: $versionCode")
+            }
         }
 
-        // 로그 저장 및 Firebase에 업로드
-        saveLogcat(logStringBuilder.toString())
+        // 모든 앱 로그 저장 및 Firebase에 업로드
+        saveLogcat(logStringBuilder.toString(), "logcat_all_apps")
+        // 구글 플레이 스토어에서 설치된 앱 로그 저장 및 Firebase에 업로드
+        saveLogcat(googlePlayLogStringBuilder.toString(), "logcat_google_play_apps")
     }
 
-    private fun saveLogcat(log: String) {
+    private fun saveLogcat(log: String, fileName: String) {
         // 타임스탬프 생성
         val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
 
@@ -159,7 +176,7 @@ class MainActivity : AppCompatActivity() {
             logsDir.mkdirs()
         }
 
-        val logFile = File(logsDir, "logcat_$timestamp.txt")
+        val logFile = File(logsDir, "${fileName}_$timestamp.txt")
 
         try {
             FileOutputStream(logFile, true).use { output ->
